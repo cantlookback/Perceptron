d7e85e7338fe3a9431518d1cfcccd5f8b59cb733
cantlookback <zinakov22@gmail.com>
2023-06-21 02:36:57 +0700

    Added module for testing set

--- main.cpp
+++ main.cpp
@@ -4,34 +4,48 @@
 using namespace std;
 
 int main(){
-    dataset dataset = loadData("C:/Perceptron/data/xor.csv", 1);
+    dataset train = loadData("C:/Perceptron/data/IrisTrain.csv", 1);
+
+    unsigned INPUT_SIZE = 4;
 
     NeuralNetwork net;
 
-    net.addLayer(2, SIGMOID);
-    net.addLayer(3, SIGMOID);
-    net.addLayer(3, SIGMOID);
+    net.addLayer(INPUT_SIZE, SIGMOID);
+    net.addLayer(8, SIGMOID);
+    net.addLayer(4, SIGMOID);
     net.addLayer(1, SIGMOID);
 
-    net.compile(1, 0.1, 5000, 1);
+    net.compile(0.7, 0.1, 5000, 1);
 
-    net.fit(&dataset.data, &dataset.answers);
-    //net.print();
-    while (true){
-        vector<double> test;
+    net.fit(&train.data, &train.answers);
 
-        std::cout << "Input >>";
-        for (unsigned i = 0; i < 2; i++){
-            int x;
-            std::cin >> x;
-            test.push_back(x);
-        }
-        test.clear();
 
-        net.feedForward(&test);
+    //? DATA TEST MODULE
+    dataset test = loadData("C:/Perceptron/data/IrisTest.csv", 1);
 
-        net.output();
+    for (unsigned i = 0; i < test.data.size(); i++){
+        std::cout << "Row " << i << " testing..." << '\n';
+        net.feedForward(&(test.data[i]));
+        std::cout << net.getOut() << '\n';
+        std::cout << "True value --> [" << test.answers[i] << "]    "; 
+        if (round(net.getOut()) == test.answers[i]) std::cout << "YES";
+        std::cout << '\n';
     }
+
+
+    // vector<double> test;
+    // test.resize(INPUT_SIZE);
+    // while (true){
+    //     std::cout << "Input >>";
+    //     for (unsigned i = 0; i < INPUT_SIZE; i++){            
+    //         std::cin >> test[i];
+    //     }
+    //     net.feedForward(&test);
+        
+    //     test.clear();
+
+    //     net.output();
+    // }
 
     return 0;
 }
