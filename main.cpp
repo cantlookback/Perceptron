d92dcc1b9696b97b0b7f215ce3ca777a008ba86f
cantlookback <zinakov22@gmail.com>
2023-07-04 17:59:55 +0700

    Init

--- main.cpp
+++ main.cpp
@@ -3,35 +3,66 @@
 
 using namespace std;
 
+void normalizeData(vector<vector<double>> *data){
+    double maxDot = 0, minDot = (*data)[0][0];
+    for (unsigned i = 0; i < (*data)[0].size(); i++){
+        for (unsigned j = 0; j < data->size(); j++){
+            maxDot = ((*data)[j][i]) > maxDot ? ((*data)[j][i]) : maxDot;
+            minDot = ((*data)[j][i]) < minDot ? ((*data)[j][i]) : minDot;
+        }
+
+        for (unsigned k = 0; k < data->size(); k++){
+            (*data)[k][i] = (((*data)[k][i]) - minDot) / (maxDot - minDot);
+        }
+        maxDot = 0;
+        minDot = (*data)[0][0];
+    }
+}
+
 int main(){
-    dataset train = loadData("C:/Perceptron/data/IrisTrain31.csv", 1, 3);
+    dataset train = loadData("C:/Perceptron/data/diabetes.csv", 1, 1);
     //Iris -- 0.7, 0.1, 1000, 1 || [4, 8, 4, 1]
 
     unsigned INPUT_SIZE = train.data[0].size();
 
+    normalizeData(&train.data);
+
     NeuralNetwork net;
 
     net.addLayer(INPUT_SIZE);
-    net.addLayer(10, SIGMOID);
-    net.addLayer(6, SIGMOID);
-    net.addLayer(3, SOFTMAX);
+    net.addLayer(8, SIGMOID);
+    net.addLayer(4, SIGMOID);
+    net.addLayer(1, SIGMOID);
 
-    net.compile(0.3, 0.2, 5000, 1, categorical_crossentropy);
+    net.compile(0.7, 0.1, 100, 1, MSE);
 
     net.fit(&train.data, &train.answers);
 
+    vector<double> test;
+    test.resize(INPUT_SIZE);
+    while (true){
+        std::cout << "Input >>";
+        for (unsigned i = 0; i < INPUT_SIZE; i++){            
+            std::cin >> test[i];
+        }
+        net.feedForward(&test);
+
+        net.output();
+    }
 
     //? DATA TEST MODULE
-    dataset test = loadData("C:/Perceptron/test/IrisTest3.csv", 1, 3);
+    // dataset test = loadData("C:/Perceptron/test/IrisTest3.csv", 1, 3);
 
-    for (unsigned i = 0; i < test.data.size(); i++){
-        std::cout << "Row " << i << " testing..." << '\n';
+    // normalizeData(&test.data);
 
-        net.feedForward(&(test.data[i]));
+    // for (unsigned i = 0; i < test.data.size(); i++){
+    //     std::cout << "Row " << i << " testing..." << '\n';
 
-        std::cout << "Got -->" << *net.getOut() << '\n';
-        std::cout << "True ->" << test.answers[i] << '\n';
-    }
+    //     net.feedForward(&(test.data[i]));
+
+    //     std::cout << "Got -->" << *net.getOut() << '\n';
+    //     std::cout << "True ->" << test.answers[i] << '\n';
+    // }
 
     return 0;
 }
